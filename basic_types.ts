d2c2351d2a4679628d6f2e40b1f2a9079e7ce771
JanaCorsellas <jana.corsellas@estudiantat.upc.edu>
2025-02-25 14:32:50 +0100

    apunts classe

--- basic_types.ts
+++ basic_types.ts
@@ -41,26 +41,30 @@ id = 10;
 console.log(typeof id, id); // number
 id = "11";
 console.log(typeof id, id); // string
+//quan fem una petició a una API, puc esperar un JSON o un error. Per tant podem tenir aquest tipus de problema
 
+//com convertim d'un tipus a un altre.
 //  Type assertions
 let someValue: any;
 someValue = "Hello, TypeScript!";
-let strLength: number = (someValue as string).length;
+let strLength: number = (someValue as string).length; 
 
 let anotherValue: number | string;
 anotherValue = "Learning TypeScript";
-let anotherStrLength: number = (<string>anotherValue).length;
+let anotherStrLength: number = (<string>anotherValue).length; //li posem davant el tipus que volem convertir. 
+// En aquest exemple ens retorna el tamany en caràcters d'un string (un número)
 
 // Arrays of types
-const listOfNotes: number[] = [7, 10, 4];
-const listOfNotes2: Array<number> = [12, 12, 12];
-console.log(typeof listOfNotes, listOfNotes); // JSON object
+//com defineixo un array de números, tenim dues formes totalment compatibles una amb l'altre, podem utilitzar la que vulguem
+const listOfNotes: number[] = [7, 10, 4]; //forma 1
+const listOfNotes2: Array<number> = [12, 12, 12]; //forma 2
+console.log(typeof listOfNotes, listOfNotes); // JSON object, ens retorna JSON (quasevol cosa complexe torna JSON, ja que no és capaç de diferenciar-ho)
 console.log(typeof listOfNotes2, listOfNotes2); // JSON object
 
 const listOfStrings: string[] = ["hello", "how are you"];
 const listOfStrings2: Array<string> = ["hola", "que tal"];
 
-const listOfAny: any[] = [1, true, "free"];
+const listOfAny: any[] = [1, true, "free"]; //en altres llenguatges no podriem fer un potipoti de diferents tipus de dades, en JS i TS sí
 listOfAny[1] = 100;
 console.log(listOfAny); // JSON object
 
@@ -71,6 +75,8 @@ console.log(typeof strNumTuple, strNumTuple); // JSON object
 
 // JSON Objects
 
+
+//varies formes de definir objectes nous
 /// type
 type Task = {
   id: string | number
@@ -86,11 +92,13 @@ const task: Task = {
 console.log(typeof task, task); // JSON object
 
 /// interface
+//defineixo una interfaç i després creo un objecte amb aquesta interfaç. Interfaç no és un tipus com a tal.
 interface Product {
   name: string;
   readonly price: number; // readonly is a modifier
   description?: string;
 }
+//un mateix objecte JSON pot complir amb diferents interfaç a la vegada
 
 const product: Product = {
   name: "laptop",
@@ -101,7 +109,8 @@ const product: Product = {
 console.log(typeof product, product); // JSON object
 
 // enums
-
+//són un conjunt de valors constants que poden ser assignats a una variable. 
+//per exemple jo vull que un objecte JSON tingui un estat, i aquest estat només pot ser un d'aquests tres valors.
 enum Status {
   Activo,     // 0
   Inactivo,   // 1
@@ -112,7 +121,7 @@ let estadoActual: Status = Status.Activo;
 console.log(estadoActual); // 0
 console.log(Status[0]);    // "Activo"
 
-
+//exemple molt clar de com utilitzar enums
 enum HttpStatus {
   OK = 200,
   NotFound = 404,
@@ -132,3 +141,10 @@ enum RolUsuario {
 let rol: RolUsuario = RolUsuario.Admin;
 console.log(rol); // "ADMIN"
 
+//tipos genèrics
+function identidad<T>(arg: T): T{
+  return arg;
+}
+
+let resultado1: string = identidad<string>("Hola");
+let resultado2: number = identidad<number>(123);
